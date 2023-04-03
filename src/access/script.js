4ad5fd6842e53c4df1e64857ea669c9d6e5ce144
Min Khant (M-Flix) <87593776+GVCFLIX@users.noreply.github.com>
2023-04-03 22:31:08 +0630

    Add files via upload

--- src/access/script.js
+++ src/access/script.js
@@ -1,6 +1,7 @@
+// console.log('hello');
 const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector("ul");
-const navLink = document.querySelectorAll("a");
+const navMenu = document.querySelector(".mk-nav-menu");
+const navLink = document.querySelectorAll(".mk-nav-link");
 
 hamburger.addEventListener("click", mobileMenu);
 navLink.forEach(n => n.addEventListener("click", closeMenu));
@@ -14,3 +15,14 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+
+
+
+
+
+// auto year copyright section
+let date = new Date();
+let year = date.getFullYear();
+console.log(date,year);
+document.getElementById("autoyear").textContent = year;
